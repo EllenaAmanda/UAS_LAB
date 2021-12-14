83e3102ed28cc6b9c935c6d8a6be092687d7afca
EllenaAmanda <76916918+EllenaAmanda@users.noreply.github.com>
2021-12-15 02:13:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,159 @@
+<?php
+  require 'vendor/autoload.php';
+  require_once('sparqllib.php');
+
+  $db = sparql_connect("http://localhost:3030/labpw/sparql");
+  if( !$db ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
+
+  sparql_ns("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+  sparql_ns("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+  sparql_ns("dbo", "http://dbpedia.org/ontology/");
+  sparql_ns("dbr", "http://dbpedia.org/resource/");
+  sparql_ns("foaf", "http://xmlns.com/foaf/0.1/");
+?>
+
+<html>
+    <head>
+        <title>List Indonesian Singers</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <!--Link ke Bootstrap-->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <!--Akhir link ke Bootstrap-->
+        <link rel="stylesheet" href="css2.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery.redirect@1.1.4/jquery.redirect.min.js"></script>
+        <!--CSS-->
+            <style>
+                .amiri{
+                    font-family: 'Amiri', serif;
+                    margin: auto;
+                    font-size: 20px;
+                }
+                
+                
+                }
+                table,tr,td{
+                    padding: 10px;
+                    font-size: 23px;
+                }
+                .bg-light{
+                    background-color: rgba(255,255,255,0.8)!important;
+                    transition:0.5s ease;
+                }
+                
+                p{
+                    text-align: right;
+                    font-family: 'Raleway', sans-serif;
+                }
+
+
+                body{
+                    background-image: url('bg.jpg');
+                    
+                }
+
+                .newtable{
+                    padding: 10px 8px;
+                    height: 280px;
+                    overflow-y: auto;
+                    
+                }
+            </style>
+        <!--Akhir CSS-->
+    </head>
+    <body>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
+            <div class="container-fluid">
+                <h2 class="amiri ms-5 my-2 fs-2">List Indonesian Singers</h2>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                </ul>
+              </div>
+            </div>
+          </nav>
+        <!--Akhir Navbar-->
+
+        <?php
+            $sparql = "
+            PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX dbo:  <http://dbpedia.org/ontology/>
+            PREFIX cat:  <http://dbpedia.org/property/>
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+            PREFIX dc:   <http://purl.org/dc/elements/1.1/>
+            PREFIX dbp: <http://dbpedia.org/property/>
+            SELECT DISTINCT *
+            WHERE
+            {
+                ?singer foaf:image ?wiki;
+                        dbp:name ?Name;
+                        dbo:birthDate ?BirthDate;
+                        dbo:birthPlace ?BirthPlace;
+                        dbo:activeYearsStartYear ?ActiveYearsStart;
+                        rdfs:comment ?Comment.
+            }";
+            $result = sparql_query($sparql);
+            //var_dump($result);
+            $fields = sparql_field_array($result);
+            $i = 0;
+
+            \EasyRdf\RdfNamespace::setDefault('og');
+        ?>
+
+        
+        <br>
+        <br>
+        <br>
+        <div class="container table-container my-3 p-0">
+            <table class="table table-light table-bordered m-0 p-0">
+                <thead class="thead-dark">
+                    <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Thumbnail</th>
+                    <?php foreach($fields as $field) : 
+                        $i++;
+                        if($i<=2) continue;
+                        echo "<th scope='col'>".$field."</th>";
+                    
+                    endforeach;?>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php 
+                $i = 1;
+                while($row = sparql_fetch_array($result)) : 
+                    $j = 0;
+                    
+                    $img_file = \EasyRdf\Graph::newAndLoad($row['wiki']);
+                ?>
+                    <tr>
+                    <th scope="row"><?= $i ?></th>
+                    <?php foreach($fields as $field){ 
+                        $j++;
+                        if($j==1) continue;
+                        if($j==2){
+                            echo "<td><img src='".$img_file->image."' class=' mx-auto cover-img'></img></td>";
+                            continue;
+                        }
+                        echo "<td>".$row[$field]."</td>";
+                    }
+                    ?>
+                    </tr>
+                <?php
+                $i++;
+                endwhile;
+                ?>
+                </tbody>
+            </table>
+            </div>
+        
+    </body>
+</html>
